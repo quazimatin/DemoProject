2b66fdb4fb04621e19a860cfab93a58de627fac2
quazimatin <quazinf@gmail.com>
2019-10-20 17:04:44 -0400

    update again

--- src/basic/SalesReceipt.java
+++ src/basic/SalesReceipt.java
@@ -89,7 +89,7 @@ public class SalesReceipt{
 			System.out.println("NetAmountPays                 : "+ netAmountPay);
 
 			System.out.println("***************************************************");
-			System.out.println("******************ARIF AND QUAZI*********************************");
+			
 			
 
 		}
